7a811c7e285c14f2b72dc71e070a4067f23f1473
Vinodkumar8660 <vinodkumarthotakura2112@gmail.com>
2022-06-12 23:10:27 +0530

    Updates

---
+++ precache-manifest.d0ea16a6e56590eb944dba5481e4ebdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80008000c6734d4620aa",
+    "url": "/shopping/static/css/main.2c33ae48.chunk.css"
+  },
+  {
+    "revision": "80008000c6734d4620aa",
+    "url": "/shopping/static/js/main.80008000.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/shopping/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "eeccf704ae7c6c47d957",
+    "url": "/shopping/static/js/2.eeccf704.chunk.js"
+  },
+  {
+    "revision": "64a952119a64ee736ee1",
+    "url": "/shopping/static/js/runtime~main.64a95211.js"
+  },
+  {
+    "revision": "cc7d5c9087c2f745e27756c6cf520f21",
+    "url": "/shopping/static/media/shop7.cc7d5c90.jpg"
+  },
+  {
+    "revision": "b658f8846b3fffd40c9ec7b301a22071",
+    "url": "/shopping/static/media/amb2.b658f884.jpg"
+  },
+  {
+    "revision": "ad07a7dcc6a30b3ac5160191a3d0dbf9",
+    "url": "/shopping/index.html"
+  }
+];
